25192c76c72fd0121685de4aa1e452c42797f856
Y <tominhy77@gmail.com>
2022-06-30 11:52:01 +0700

    xay dung Features

--- js/theme-change.js
+++ js/theme-change.js
@@ -14,5 +14,6 @@ document.getElementById("switchButton").onclick = function () {
     // dùng classlist để gọi thuộc tính class của thẻ body
     //Sử dụng toggle() để thêm hoặc xóa 1 class của thẻ html. Nếu thẻ body không có class dark thì toggle sẽ thêm class dark vào body. Ngược lại, nếu thẻ body đang có class dark thì toogle sẽ xóa class dark đi.
     document.getElementById("myBody").classList.toggle("dark");
+    document.getElementById("myCategories").classList.toggle("dark");
   };
   
